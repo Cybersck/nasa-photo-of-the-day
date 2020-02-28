4fe02c9c7d7fcfb824c2335a9fd9dacce118cb8e
kevin lautermilch <kevin.laut@outlook.com>
2020-02-28 14:39:41 -0500

    Updates

---
+++ precache-manifest.42f8cd33e4e23f160ac2e7ef765da80c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad120b7e82d28197acdd1beec00cc48a",
+    "url": "/Nasa-Photo-Of-The-Day/index.html"
+  },
+  {
+    "revision": "9252cc57f30466ec0ca3",
+    "url": "/Nasa-Photo-Of-The-Day/static/css/2.2f4f45f5.chunk.css"
+  },
+  {
+    "revision": "0fd6e3cf2101c989af96",
+    "url": "/Nasa-Photo-Of-The-Day/static/css/main.19ccae34.chunk.css"
+  },
+  {
+    "revision": "9252cc57f30466ec0ca3",
+    "url": "/Nasa-Photo-Of-The-Day/static/js/2.5db35602.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Nasa-Photo-Of-The-Day/static/js/2.5db35602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd6e3cf2101c989af96",
+    "url": "/Nasa-Photo-Of-The-Day/static/js/main.37e7f7e3.chunk.js"
+  },
+  {
+    "revision": "492d01c512e0112af8a8",
+    "url": "/Nasa-Photo-Of-The-Day/static/js/runtime-main.c37167eb.js"
+  },
+  {
+    "revision": "695ad38a234c5de4d3686e086b84e246",
+    "url": "/Nasa-Photo-Of-The-Day/static/media/bg.695ad38a.jpg"
+  }
+]);
